d3132f924ccae2b28245e0441c5ab6a84e1b6052
Chung Wei Leong <15154097+chungweileong94@users.noreply.github.com>
2025-03-07 22:07:17 +0800

    Extends session expiration on GET request only (#116)

--- middleware.ts
+++ middleware.ts
@@ -15,7 +15,7 @@ export async function middleware(request: NextRequest) {
 
   let res = NextResponse.next();
 
-  if (sessionCookie) {
+  if (sessionCookie && request.method === "GET") {
     try {
       const parsed = await verifyToken(sessionCookie.value);
       const expiresInOneDay = new Date(Date.now() + 24 * 60 * 60 * 1000);
